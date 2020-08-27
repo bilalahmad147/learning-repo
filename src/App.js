bb24f280dd4776f84d1ca30a2a241f0162ade85f
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-08-27 03:42:41 -0800

    deleted practice app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header';
-import Balance from './Components/Balance';
-import History from './Components/History';
-import Transaction from './Components/Transaction';
-import Expense from './Components/Expense';
-import { TransProvider } from './Components/TransContext';
 
 
 const App = () => {
-  
+
   return (
-    <TransProvider>
-      <div className="App">
-        <Header />
-        <Balance />
-        <Expense />
-        <History />
-        <Transaction />
-      </div>
-    </TransProvider>
+    <h1>hello world</h1>
   );
 }
 
