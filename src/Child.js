8940ac76ac7f72ed7a55e57634d084b3ea4873ae
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-08-29 03:01:28 -0800

    added useReducer Method

--- src/Child.js
+++ src/Child.js
@@ -1,19 +1,19 @@
-// import React, { useReducer } from 'react';
-// import counterReducer from './CounterReducer';
+import React, { useReducer } from 'react';
+import counterReducer from './CounterReducer';
 
 
-// const Child = () => {
+const Child = () => {
 
-//     let [state, dispatch] = useReducer(counterReducer,0);
-//     return (
-//         <div>
-//             <h1>this is counter using CounterReducer</h1>
-//             <h1>Counter : {state} </h1>
-//             <button onClick={() => dispatch('ADD')}>Increament</button>
-//             <button onClick={() => dispatch('SUB')}>Decreament</button>
-//             <button onClick={() => dispatch()}>Reset</button>
-//         </div>
-//     )
-// }
+    let [state, dispatch] = useReducer(counterReducer,0);
+    return (
+        <div>
+            <h1>this is counter using CounterReducer</h1>
+            <h1>Counter : {state} </h1>
+            <button onClick={() => dispatch('ADD')}>Increament</button>
+            <button onClick={() => dispatch('SUB')}>Decreament</button>
+            <button onClick={() => dispatch()}>Reset</button>
+        </div>
+    )
+}
 
-// export default Child;
+export default Child;
--- src/CounterReducer.js
+++ src/CounterReducer.js
@@ -1,12 +1,12 @@
-// const counterReducer = (state, action) => {
-//     switch (action) {
-//         case 'ADD':
-//             return state + 1;
-//         case 'SUB':
-//             return state - 1;
-//         default :
-//             return state
-//     }
-// }
+const counterReducer = (state, action) => {
+    switch (action) {
+        case 'ADD':
+            return state + 1;
+        case 'SUB':
+            return state - 1;
+        default :
+            return state
+    }
+}
 
-// export default counterReducer;
+export default counterReducer;
