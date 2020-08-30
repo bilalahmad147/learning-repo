a10efbf80a91683a19d9bd86151550116d739dc4
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-08-29 21:51:53 -0800

    Makes changes for simple react app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Child from './Child';
-import CounterContext from './CounterContext';
-import Child1 from './Child1';
 
 
 const App = () => {
 
-  let countState = useState(0);
 
   return (
-    <CounterContext.Provider value={countState}>
       <div className="App">
-        <Child />
-        <Child1 />
+        <h1>hello world</h1>
       </div>
-    </CounterContext.Provider>
   );
 }
 
--- src/CounterContext.js
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from 'react';
-
-const CounterContext = createContext(5);
-
-export default CounterContext;
