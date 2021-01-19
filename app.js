d792f7d8333b0b474ff69d66b6a26c7783fcd621
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-01-20 03:39:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,22 @@
 //     alert('Name is not correct')
 // }
 
-var names = [1,2,3,4,5]
-for (var i=0; i<5; i++){
-    console.log(names)
+// var names = [1,2,3,4,5]
+// for (var i=0; i<5; i++){
+//     console.log(names)
+// }
+
+// var todatDate = new Date()
+// console.log(todatDate.getFullYear())
+// console.log(todatDate.getHours())
+// console.log(todatDate.getTime())
+
+function getCurrentTime () {
+    var now = new Date();
+    var hours = now.getHours();
+    var min = now.getMinutes();
+    var time = 'hours are ' + hours + " mintues are " + min;
+    console.log(time)
 }
+
+getCurrentTime()
