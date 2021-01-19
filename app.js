66f9e26570b17ed2263082935274c2ce96362737
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-01-19 23:32:40 +0500

    updated js

---
+++ app.js
@@ -0,0 +1,13 @@
+// alert('bilal ahmad')
+
+// var name = prompt("Enter userName: ")
+// if (name === 'bilal') {
+//     alert("Name is correct")
+// } else {
+//     alert('Name is not correct')
+// }
+
+var names = [1,2,3,4,5]
+for (var i=0; i<5; i++){
+    console.log(names)
+}
