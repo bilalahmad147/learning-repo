a192c958a19906901ccfbbfe1f1fa26bf8f2b606
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-01-20 18:59:01 +0500

    final commit for react boiler plate

--- react-boiler-plate/src/App.js
+++ react-boiler-plate/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var stuff = { name: 'dani', age: 21, shoes: 'nike' }
+  var stuff1 = { name: 'dani', age: 21, clothes: { shirt: 'white' } };
+  var mergeObj = { ...stuff, ...stuff1 };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{mergeObj.name}</h3>
     </div>
   );
 }
