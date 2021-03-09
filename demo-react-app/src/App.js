2d6787979cd03deb4fed0948d0b19bd55231daa4
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-03-10 01:44:17 +0500

    updated changes

--- demo-react-app/src/App.js
+++ demo-react-app/src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <img width='100%' height='auto' src="https://i.gifer.com/N8Zj.gif" alt="header"/>
-      </div>
-      <h1>hello world</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ demo-react-app/src/Components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+function Header() {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Sab Tech</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#features">About Us</Nav.Link>
+                        <Nav.Link href="#pricing">Projects</Nav.Link>
+                        <Nav.Link href="#deets">Contact Us</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+}
+
+export default Header;
