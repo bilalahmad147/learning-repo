d3ec9707e526e097ac7d1a5361bcc481323a2ce1
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-06-08 14:38:47 +0500

    UPDATED CHANGES

--- node-project1/server.js
+++ node-project1/server.js
@@ -1,9 +1,19 @@
-const http = require("http");
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
 
-const server = http.createServer((req, res) => {
-  console.log("request made");
-});
-
-server.listen(3000, "localhost", () => {
-  console.log("listening for request from 3000");
-});
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var fileName = "." + q.pathname;
+    fs.readFile(fileName, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
